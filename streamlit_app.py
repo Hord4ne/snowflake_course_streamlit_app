5825c3d102ea28176671c5170ec8d0ee438022ad
Kamil Mazur <maz.kamil@gmail.com>
2022-11-07 18:38:44 +0100

    added presentation of normalized data

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalize
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# visualize
+streamlit.dataframe(fruityvice_normalized)
